56155ae82dd55b9f185298c05832c283bc3bf9f2
ganga2006 <166795847+ganga2006@users.noreply.github.com>
2025-05-15 17:16:55 +0530

    Create main.c

---
+++ main/main.c
@@ -0,0 +1,24 @@
+#include "stdio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+
+#define LED_PIN GPIO_NUM_2
+
+void app_main(void) {
+
+    gpio_reset_pin(LED_PIN);
+    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
+    printf("ESP32 is started! blinking LED");
+
+
+    while (1)
+    {
+        gpio_set_level(LED_PIN, 1);
+        vTaskDelay(500/portTICK_PERIOD_MS);
+
+        gpio_set_level(LED_PIN, 0);
+        vTaskDelay(500/portTICK_PERIOD_MS);
+    }
+    
+}
